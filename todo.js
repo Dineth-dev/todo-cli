9a0d635b3caa076f4273874837c729aa8235ab58
Dineth Gunawardena <dineth.gunawardena@jibb.ai>
2025-04-23 20:40:21 +1000

    refactor: simplify todo.js to use tasks module

--- todo.js
+++ todo.js
@@ -3,47 +3,77 @@ const args = process.argv.slice(3); // Removes first 3 inputs
 file_name = 'tasks.json';
 const { listTasks, addTask, markTaskAsDone, markTaskAsIncomplete, deleteTask, sortTasksByCompletion } = require("./tasks.js");
 
-if(command == 'list'){
-    listTasks();
-}
-else if(command == 'add'){
-    taskText = args.join(" ");// Adds a space between each argument before joining it all into one
-    if(taskText){
-        addTask(taskText);
-    }else{
-        console.log("Please provide a task description")
-    }
-}else if(command == 'done'){
-    taskNumber = parseInt(args[0]);
-    if(isNaN(taskNumber) || taskNumber < 1){
-        console.log('Please enter a valid task number');
-    }else{
-        markTaskAsDone(taskNumber);
-    }
-}
-else if(command == 'undone'){
-    taskNumber = parseInt(args[0]);
-    if(isNaN(taskNumber) || taskNumber < 1){
-        console.log('Please enter a valid task number');
-    }else{
-        markTaskAsIncomplete(taskNumber);
-    }
-}else if(command == 'delete'){
-    taskNumber = parseInt(args[0]);
-    if(isNaN(taskNumber) || taskNumber < 1){
-        console.log('Please enter a valid task number');
-    }else{
-        deleteTask(taskNumber);
-    }
-}else if(command == 'sort'){
-    const sortedTasks = sortTasksByCompletion();
-    console.log("Sorted Tasks: ");
-    sortedTasks.forEach((task,index) => {
-        const status = task.done ? "[Done]" : "[Not Completed]";
-        console.log(`${index + 1}. ${task.text} ${status}`);
-    });
-}
+// if(command == 'list'){
+//     listTasks();
+// }
+// else if(command == 'add'){
+//     taskText = args.join(" ");// Adds a space between each argument before joining it all into one
+//     if(taskText){
+//         addTask(taskText);
+//     }else{
+//         console.log("Please provide a task description")
+//     }
+// }else if(command == 'done'){
+//     taskNumber = parseInt(args[0]);
+//     if(isNaN(taskNumber) || taskNumber < 1){
+//         console.log('Please enter a valid task number');
+//     }else{
+//         markTaskAsDone(taskNumber);
+//     }
+// }
+// else if(command == 'undone'){
+//     taskNumber = parseInt(args[0]);
+//     if(isNaN(taskNumber) || taskNumber < 1){
+//         console.log('Please enter a valid task number');
+//     }else{
+//         markTaskAsIncomplete(taskNumber);
+//     }
+// }else if(command == 'delete'){
+//     taskNumber = parseInt(args[0]);
+//     if(isNaN(taskNumber) || taskNumber < 1){
+//         console.log('Please enter a valid task number');
+//     }else{
+//         deleteTask(taskNumber);
+//     }
+// }else if(command == 'sort'){
+//     const sortedTasks = sortTasksByCompletion();
+//     console.log("Sorted Tasks: ");
+//     sortedTasks.forEach((task,index) => {
+//         const status = task.done ? "[Done]" : "[Not Completed]";
+//         console.log(`${index + 1}. ${task.text} ${status}`);
+//     });
+// }
 
+<<<<<<< HEAD
 else{
     console.log("Unknown command");
+=======
+// else{
+//     console.log("Unknown command");
+// }
+
+switch(command){
+    case "add":
+        const taskText = args.join(" ");
+        if (taskText){
+            addTask(taskText);
+        } else{
+            console.log("Please provide a task.");
+        }
+        break;
+    case "list":
+        listTasks();
+        break;
+    case "done":
+        markTaskAsDone(parseInt(args[0]));
+        break;
+    case "undone":
+        markTaskAsIncomplete(parseInt(args[0]));
+        break;  
+    case "sort":
+        sortTasksByCompletion();
+        break;
+    default:
+        console.log("Unknown command");
+>>>>>>> 1222344 (refactor: simplify todo.js to use tasks module)
 }
